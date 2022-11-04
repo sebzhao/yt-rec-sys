7ec945021a5294b74e23b0a5c8df154b9da3311c
Sebastian Zhao <sebbyzhao@berkeley.edu>
2022-11-03 23:57:06 -0700

    Squashed commit to remove pickle files

--- utils.py
+++ utils.py
@@ -4,9 +4,23 @@ import pandas as pd
 from networkx.algorithms import approximation
 from pyvis.network import Network
 
-def visualize_subnetwork(network, node_id):
+def most_popular_nodes(G, num_nodes = 1):
     """
-    network: networkx object of full graph
-    node_id: 
+    Arguments
+        G: networkx object of full graph
+        num_nodes: number of nodes to return
+    Return
+        A list of the most popular nodes in decreasing order (first subnetwork is the largest)
     """
+    degree_dict = nx.in_degree_centrality(G)
+    sorted_nodes = list(sorted(degree_dict, key=lambda x: degree_dict[x], reverse=True)) 
+    return sorted_nodes[:num_nodes]
+        
+def add_attr_from_pandas(G, df, id, columns):
+    for i in G:
+        row = df[df[id] == i]
+        for column in columns:
+            G.nodes[i][column] = row[column].iloc[0]
 
+def get_node_attributes(G, attribute):
+    return [G.nodes[node][attribute] for node in G]
